1f27590a115d662ae965a17124702e8e08259614
Muhammad Umar <u88f33@gmail.com>
2026-03-18 15:47:47 +0500

    Added controllers, models, middlewares, routes and views folders in src and javascript and css in public

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 
 // Initializing Environment variables from ".env" file
@@ -6,8 +8,23 @@ dotenv.config();
 
 const app = express();
 
+// Fix __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Set views folder
+app.set("views", path.join(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+// Middleware for JSON and HTML Form data
+app.use( express.urlencoded( { extended: false } ) );
+app.use( express.json() );
+
+// Route
 app.get( "/", ( req, res, next ) => {
-    res.send( `<h1 style='color:red;font-size:80px;'>Novameds</h1>` )
+    res.render( 
+        "show/show"
+    )
 } );
 
 const PORT = process.env.PORT;
