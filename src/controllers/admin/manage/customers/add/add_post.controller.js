7baa2f9829d9994cf8eb93653a13006413436692
Muhammad Umar <u88f33@gmail.com>
2026-03-31 15:12:29 +0500

    Changes done

---
+++ src/controllers/admin/manage/customers/add/add_post.controller.js
@@ -0,0 +1,10 @@
+const AddCustomerRecordCtrlPost = ( req, res, next ) => {
+    console.log( "++++++++++++++++++++++++++++++++" );
+    console.log( "Customer Record Added" );    
+    console.log( "++++++++++++++++++++++++++++++++" );
+    console.log( req.body );
+    console.log( "++++++++++++++++++++++++++++++++" );
+    res.redirect( "/admin/manage/customers/add" );
+}
+
+export default AddCustomerRecordCtrlPost;
--- src/routes/admin/manage/customers/add.route.js
+++ src/routes/admin/manage/customers/add.route.js
@@ -2,8 +2,13 @@ import express from "express";
 import AddCustomerRecordCtrl 
 from "../../../../controllers/admin/manage/customers/add/add_get.controller.js"
 
+import AddCustomerRecordCtrlPost
+from "../../../../controllers/admin/manage/customers/add/add_post.controller.js"
+
+
 const router = express.Router();
 
 router.get( "/", AddCustomerRecordCtrl );
+router.post( "/", AddCustomerRecordCtrlPost );
 
 export default router;
