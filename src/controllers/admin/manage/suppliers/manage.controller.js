5b74ca5e08e28f89e50b992b02da6a0a326d58cf
Muhammad Umar <u88f33@gmail.com>
2026-04-05 16:22:32 +0500

    Changes done

--- src/controllers/admin/manage/suppliers/manage.controller.js
+++ src/controllers/admin/manage/suppliers/manage.controller.js
@@ -1,6 +1,36 @@
-const ManageSupplierRecordsCtrl = ( req, res, next ) => {
+import SuppliersCollection from "../../../../models/suppliers.model.js";
+
+
+const ManageSupplierRecordsCtrl = async ( req, res, next ) => {
+
+    const page = Number.parseInt(req.query.page) || 1;
+    const limit = Number.parseInt(req.query.limit) || 5;
+    const skip = ( page - 1 ) * limit;
+    const totalSupplierRecords = await SuppliersCollection.countDocuments();
+    const totalPages =  Math.ceil( totalSupplierRecords / limit );
+    
+    console.log( `page: `, page );
+    console.log( `limit: `, limit );
+    console.log( `Supplier records: `, totalSupplierRecords );
+    console.log( `Total Pages: `, totalPages );
+
+
+    // Return all the documents in a "suppliers" collection
+    const suppliersRecordsInDatabase =
+    await SuppliersCollection.find()
+    .skip( skip )
+    .limit( limit )
+    .sort( { createdAt: -1 } );
+
+
+
     res.render(
-        "admin/manage/suppliers/manage"
+        "admin/manage/suppliers/manage",
+        {
+            suppliersRecordsInDatabase,
+            currentPage: page,
+            totalPages
+        }
     );
 }
 
