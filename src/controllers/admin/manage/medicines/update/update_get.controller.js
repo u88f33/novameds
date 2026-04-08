7f0f6a4ebf2dbd956b579e8601e2ef703dd20d68
Muhammad Umar <u88f33@gmail.com>
2026-04-08 13:11:11 +0500

    Changes done

--- src/controllers/admin/manage/medicines/update/update_get.controller.js
+++ src/controllers/admin/manage/medicines/update/update_get.controller.js
@@ -1,6 +1,24 @@
-const UpdateMedicineRecordCtrl = ( req, res, next ) => {
+import SuppliersCollection from "../../../../../models/suppliers.model.js";
+import MedicinesCollection from "../../../../../models/medicines.model.js";
+
+
+const UpdateMedicineRecordCtrl = async ( req, res, next ) => {
+
+    const suppliersRecordsFromDB = await SuppliersCollection
+    .find();
+
+    const medicineRecordFromDB = await MedicinesCollection
+    .findById( req.params.id )
+    .populate("supplierId");
+
+    console.log( medicineRecordFromDB );
+
     res.render(
-        "admin/manage/medicines/update"
+        "admin/manage/medicines/update",
+        {
+            suppliersRecordsFromDB,
+            medicineRecordFromDB
+        }
     )
 }
 
