307c4edc92037b98ff552c9d18de3bf9921b0455
Muhammad Umar <u88f33@gmail.com>
2026-04-11 17:44:32 +0500

    Changes done on April 11, 2026 at 5:44 PM

--- src/controllers/admin/manage/customers/update/update_post.controller.js
+++ src/controllers/admin/manage/customers/update/update_post.controller.js
@@ -1,3 +1,6 @@
+import CustomerCollection from "../../../../../models/customers.model.js";
+import bcrypt from "bcryptjs";
+
 const UpdateCustomerRecordCtrlPost = ( req, res, next ) => {
     const {
         customer_name,
@@ -10,15 +13,19 @@ const UpdateCustomerRecordCtrlPost = ( req, res, next ) => {
         customer_country
     } = req.body;
 
+    if ( customer_password == customer_confirm_password ) {
+        
+    }
 
-    console.log( customer_name );
-    console.log( customer_email );
-    console.log( customer_password );
-    console.log( customer_confirm_password );
-    console.log( customer_phone );
-    console.log( customer_address );
-    console.log( customer_city );
-    console.log( customer_country );
+    const updatedCustomerRecord = {
+        customerName: customer_name,
+        customerEmail: customer_email,
+        customerPassword: customer_password,
+        customerPhone: customer_phone,
+        customerAddress: customer_address,
+        customerCity: customer_city,
+        customerCountry: customer_country
+    }
 }
 
 export default UpdateCustomerRecordCtrlPost;
