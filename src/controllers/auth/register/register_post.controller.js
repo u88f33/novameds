c0245c85ee3231143bf08c6ec48a03fff0e0918f
Muhammad Umar <u88f33@gmail.com>
2026-04-12 20:34:20 +0500

    Changes done on April 12, 2026 on 8:34 PM

--- src/controllers/auth/register/register_post.controller.js
+++ src/controllers/auth/register/register_post.controller.js
@@ -1,23 +1,33 @@
 import CustomerCollections from "../../../models/customers.model.js";
+import bcrypt from "bcryptjs";
 
 const RegisterPostCtrl = async ( req, res, next ) => {
 
     try {
 
+        let hashedPassword;
+
         const {
             user_name,
             user_email,
             user_password,
+            confirm_user_password,
             user_phone,
             user_address,
             user_city,
             user_country
         } =  req.body;
 
+        if ( user_password != confirm_user_password ) {
+            res.redirect( "/register/?errorMessage=Password do not match" );
+        } else {
+            hashedPassword = await bcrypt.hash( user_password, 10 );
+        }
+
         const userRecord = {
             customerName: user_name,
             customerEmail: user_email,
-            customerPassword: user_password,
+            customerPassword: hashedPassword,
             customerPhone: user_phone,
             customerAddress: user_address,
             customerCity: user_city,
