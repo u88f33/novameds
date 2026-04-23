b4ac1f9ad32f75bbb5babf166b98740648feea41
Muhammad Umar <u88f33@gmail.com>
2026-04-23 17:11:03 +0500

    Changes done

--- src/routes/user/cart.route.js
+++ src/routes/user/cart.route.js
@@ -3,10 +3,12 @@ import CartPageCtrl from "../../controllers/user/cart/cart.controller.js";
 import CartItemAddCtrl from "../../controllers/user/cart/cart_post.controller.js";
 import CartItemDeleteCtrl from "../../controllers/user/cart/cart_delete.controller.js";
 import CartItemsApiCtrl from "../../controllers/user/cart/cart_api.controller.js";
+import CheckoutPageCtrl from "../../controllers/user/cart/checkout.controller.js";
 
 const router = express.Router();
 
 router.get( "/", CartPageCtrl );
+router.get( "/checkout", CheckoutPageCtrl )
 router.post( "/add/:id", CartItemAddCtrl );
 router.delete( "/delete/:id", CartItemDeleteCtrl );
 router.get( "/api", CartItemsApiCtrl );
