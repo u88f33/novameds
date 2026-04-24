392aa55efb99727764027642e22ee82e8dd67d43
Muhammad Umar <u88f33@gmail.com>
2026-04-24 19:47:03 +0500

    Changes done

--- public/js/scripts/orders/order.js
+++ public/js/scripts/orders/order.js
@@ -4,16 +4,29 @@ const boxes = document.querySelectorAll( ".payment-box__info" );
 boxes[0].style.display = "block";
 
 async function fetchCustomerAddress( userId ) {
-    const getUserAddress = await fetch( `/apis/api/customer/address/${ userId }` );
-    const response = await getUserAddress.json();
+    try {
+        const getUserAddress = await fetch( `/apis/api/customer/address/${ userId }` );
+        const response = await getUserAddress.json();
 
-    return response;
+        return response;
+    } catch ( error ) {
+        console.log( "Error: " + error );
+    }
 }
 
-async function fillShippingForm( userId, inputId, inputValue ) {
-    const user = await fetchCustomerAddress( userId );
+async function fillShippingAddress( userId = "", inputId = "", inputValue = "" ) {
+    try {
+        const user = await fetchCustomerAddress( userId );
 
-    document.getElementById( inputId ).value = user[inputValue];
+        if ( inputValue ) {
+            document.getElementById( inputId ).value = user[inputValue];
+        } else {
+            document.getElementById( inputId ).value = ""
+        }
+
+    } catch ( error ) {
+        console.log( "Error: " + error );
+    }
 }
 
 radios.forEach( radio => {
@@ -32,16 +45,18 @@ radios.forEach( radio => {
     } )
 } );
 
-fillShippingForm( userId, "perm_address", "customerAddress" );
-fillShippingForm( userId, "perm_city", "customerCity" );
-fillShippingForm( userId, "perm_state", "customerCountry" );
+fillShippingAddress( userId, "perm_address", "customerAddress" );
+fillShippingAddress( userId, "perm_city", "customerCity" );
+fillShippingAddress( userId, "perm_state", "customerCountry" );
 
 document.getElementById( "same_address" ).addEventListener( "change", function() {
     if ( this.checked ) {
-        fillShippingForm( userId, "perm_address", "customerAddress" );
-        fillShippingForm( userId, "perm_city", "customerCity" );
-        fillShippingForm( userId, "perm_state", "customerCountry" );
+        fillShippingAddress( userId, "ship_address", "customerAddress" );
+        fillShippingAddress( userId, "ship_city", "customerCity" );
+        fillShippingAddress( userId, "ship_state", "customerCountry" );
     } else {
-
+        fillShippingAddress( userId, "ship_address", "" );
+        fillShippingAddress( userId, "ship_city", "" );
+        fillShippingAddress( userId, "ship_state", "" );   
     }
 })
