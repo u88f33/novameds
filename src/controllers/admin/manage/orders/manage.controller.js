1d724505a9899210012bb18b786aa9136b72bdd6
Muhammad Umar <u88f33@gmail.com>
2026-05-01 16:22:15 +0500

    Chagnes done

--- src/controllers/admin/manage/orders/manage.controller.js
+++ src/controllers/admin/manage/orders/manage.controller.js
@@ -2,15 +2,30 @@ import OrdersCollection from "../../../../models/order.model.js";
 
 const ManageOrderRecordsCtrl = async ( req, res, next ) => {
 
-    const customersOrder = await OrdersCollection.find()
-    .populate( "customerId" );
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+
+    const customersOrder = await OrdersCollection.paginate(
+        {},
+        {
+            page,
+            limit,
+            populate: "customerId"
+        }
+    )
     
     console.log( customersOrder );
 
     res.render(
         "admin/manage/orders/manage",
         {
-            customersOrder
+            customersOrder: customersOrder.docs,
+            totalPages: customersOrder.totalPages,
+            currentPage: customersOrder.page,
+            hasPrevPage: customersOrder.hasPrevPage,
+            hasNextPage: customersOrder.hasNextPage,
+            prevPage: customersOrder.prevPage,
+            nextPage: customersOrder.nextPage
         }
     )
 }
