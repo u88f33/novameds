bb15dab1d8f171a91b000c8c67c59573deb71b05
Muhammad Umar <u88f33@gmail.com>
2026-05-02 13:10:50 +0500

    Changes done

--- src/controllers/admin/manage/orders/view.controller.js
+++ src/controllers/admin/manage/orders/view.controller.js
@@ -1,6 +1,18 @@
-const ViewOrderRecordCtrl = ( req, res, next ) => {
+import OrderCollection from "../../../../models/order.model.js";
+
+const ViewOrderRecordCtrl = async ( req, res, next ) => {
+
+    const customerOrder = await OrderCollection.findById( req.params.id )
+    .populate( "customerId" )
+    .populate( "items.medicineId" );
+
+    console.log( customerOrder );
+
     res.render(
-        "admin/manage/orders/view"
+        "admin/manage/orders/view",
+        {
+            customerOrder
+        }
     )
 }
 
