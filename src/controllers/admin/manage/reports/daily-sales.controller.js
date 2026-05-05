52d8ebc44b9e1ea2dc33a4bb576984e9204c8620
Muhammad Umar <u88f33@gmail.com>
2026-05-05 17:06:06 +0500

    Changes done

--- src/controllers/admin/manage/reports/daily-sales.controller.js
+++ src/controllers/admin/manage/reports/daily-sales.controller.js
@@ -1,6 +1,53 @@
+import { fileURLToPath } from "url";
+import OrderCollection from "../../../../models/order.model.js";
+import generatePDF from "../../../../utils/salesReport/generatePdf.js";
+import path from "path";
+import fs from "fs";
+
 const DailySalesReportCtrl = async ( req, res, next ) => {
+
+    const customersOrders = await OrderCollection.aggregate([
+        {
+            $group: {
+                _id: {
+                    $dateToString: {
+                        format: "%Y-%m-%d",
+                        date: "$createdAt"
+                    }
+                }
+            }
+        }
+    ]);
+
+    const __filename__ = fileURLToPath( import.meta.url );
+    const rooDir = path.resolve( __filename__, "../../../../../../" );
+
+
+    const readSalesReportFiles = path.join( 
+        rooDir, 
+        "public", 
+        "assets", 
+        "reports", 
+        "dailySales" 
+    );
+
+    const pdfSalesReportsArray = fs.readdirSync( readSalesReportFiles );
+
+    let startingDate, endingDate;
+    customersOrders.forEach( doc => {
+        let date = doc._id;
+        startingDate = new Date( date );
+        endingDate = new Date( date );
+        endingDate.setDate( endingDate.getDate() + 1 );
+
+        generatePDF( startingDate, endingDate, res );
+    } );
+
     res.render(
         "admin/manage/reports/dailySales",
+        {
+            pdfSalesReportsArray
+        }
     )
 }
 
--- src/utils/salesReport/generatePdf.js
+++ src/utils/salesReport/generatePdf.js
@@ -4,12 +4,32 @@ import { fileURLToPath } from 'url';
 import path from "path";
 import fs from "fs";
 
-async function generatePDF() {
+function formatDate( date ) {
+    let monthNamesArray =  [
+                            "January", "February", "March", "April", 
+                            "May", "June", "July", "August", "September", 
+                            "October", "November", "December"
+                        ];
 
-    const startDate = new Date("May 4, 2026");
+    let day = date.getDate().toString().padStart( 2, "0" );
+    let month = monthNamesArray[ date.getMonth() ];
+    let year = date.getFullYear().toString().padStart( 5, "0" );
+
+    return `${month}${day}${year}`;
+}
+
+async function generatePDF( startingDate, endingDate, response ) {
+
+    let monthNamesArray =  [
+                            "January", "February", "March", "April", 
+                            "May", "June", "July", "August", "September", 
+                            "October", "November", "December"
+                        ];
+
+    const startDate = startingDate;
     startDate.setHours( 0, 0, 0 );
 
-    const endDate = new Date( "May 6, 2026" );
+    const endDate = endingDate;
     endDate.setHours( 0, 0, 0 );
 
     const dailySale = await getDailySales( startDate, endDate );
@@ -19,43 +39,114 @@ async function generatePDF() {
     const __filename__ = fileURLToPath( import.meta.url );
     const __dirname__ = path.resolve(__filename__, "../../../../"); 
 
+    const salesReportName = `${formatDate( startDate )}.pdf`;
     const salesReportsDirectory = path.join( 
         __dirname__, 
         "public", 
         "assets", 
         "reports", 
         "dailySales",
-        "dailySalesReport.pdf"
+        `${ salesReportName }`
     );
 
     doc.pipe( fs.createWriteStream( salesReportsDirectory ) );
-
-    doc
-    .text( `Sales Report of ${ new Date( dailySale[0].date ).toLocaleString("en-US") }` )
-
 
     doc.moveDown();
     doc.moveDown();
 
     const dailySalesTable = [
+        [
+            { 
+               text: `Sales Report of ${monthNamesArray[startDate.getMonth()]} ${ startDate.getDate() }, ${startDate.getFullYear()}`,
+               colSpan: "6",
+               padding: "1.3em",
+               align: { x: "center", y: "center" }
+            }
+        ],
         [   
-            'Date', 
-            'Name', 
-            'Unit Price', 
-            'Quantity', 
-            "Total Sales", 
-            "Available Stock"
+            { 
+                text: 'Date',
+                textColor: '#fff', 
+                backgroundColor: "#444",
+                align: { x: "center", y: "center" },
+                padding: "1em"
+            }, 
+            { 
+                text: 'Name',
+                textColor: '#fff', 
+                backgroundColor: "#444",
+                align: { x: "center", y: "center" },
+                padding: "1em"
+            }, 
+            { 
+                text: 'Unit Price',
+                textColor: '#fff', 
+                backgroundColor: "#444",
+                align: { x: "center", y: "center" },
+                padding: "1em"
+            },
+            { 
+                text: 'Quantity',
+                textColor: '#fff', 
+                backgroundColor: "#444",
+                align: { x: "center", y: "center" },
+                padding: "1em"
+            },
+            { 
+                text: "Total Sales",
+                textColor: '#fff', 
+                backgroundColor: "#444",
+                align: { x: "center", y: "center" },
+                padding: "1em"
+            },
+            { 
+                text: "Available Stock",
+                textColor: '#fff', 
+                backgroundColor: "#444",
+                align: { x: "center", y: "center" },
+                padding: "1em"
+            },
         ],
     ];
 
     dailySale.forEach( data => {
             let tableRow = [
-                new Date( data.date ).toLocaleDateString("en-US"),
-                data.medicineName,
-                data.unitPrice,
-                data.totalQuantity,
-                data.totalSales,
-                data.availableStock
+                { 
+                    text: `${new Date( data.date ).toLocaleDateString("en-US")}`,
+                    textColor: '#000',
+                    align: { x: "center", y: "center" },
+                    padding: "1em"
+                },
+                { 
+                    text: data.medicineName,
+                    textColor: '#000',
+                    align: { x: "center", y: "center" },
+                    padding: "1em"
+                },
+                { 
+                    text: data.unitPrice,
+                    textColor: '#000',
+                    align: { x: "center", y: "center" },
+                    padding: "1em"
+                },
+                { 
+                    text: data.totalQuantity,
+                    textColor: '#000',
+                    align: { x: "center", y: "center" },
+                    padding: "1em"
+                },
+                { 
+                    text: data.totalSales,
+                    textColor: '#000',
+                    align: { x: "center", y: "center" },
+                    padding: "1em"
+                },
+                { 
+                    text: data.availableStock,
+                    textColor: '#000',
+                    align: { x: "center", y: "center" },
+                    padding: "1em"
+                }
             ];
 
             dailySalesTable.push( tableRow );
@@ -67,7 +158,6 @@ async function generatePDF() {
 
 
         doc.end();
-    // console.log( salesReportsDirectory );
 
 }
 
