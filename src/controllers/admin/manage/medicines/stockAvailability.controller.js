b97c4047c7fefa84220270d479219ec9b0f83b58
Muhammad Umar <u88f33@gmail.com>
2026-05-09 16:57:58 +0500

    Total price (medicineStock X medicinePrice) is added in stock availability pdf report.

--- src/controllers/admin/manage/medicines/stockAvailability.controller.js
+++ src/controllers/admin/manage/medicines/stockAvailability.controller.js
@@ -12,7 +12,10 @@ const StockAvailablityReportPdfCtrl = async ( req, res, next ) => {
                 _id: 0,
                 medicineName: 1,
                 medicineStock: 1,
-                medicinePrice: 1
+                medicinePrice: 1,
+                totalPrice: {
+                    $multiply: [ "$medicineStock", "$medicinePrice" ]
+                }
             }
         }
     ]);
@@ -26,7 +29,7 @@ const StockAvailablityReportPdfCtrl = async ( req, res, next ) => {
         [
             {
                 text: `Medicine Availability Report of ${monthNames[ date.getMonth() ]} ${date.getDate()}, ${date.getFullYear()}`,
-                colSpan: "3",
+                colSpan: "4",
                 padding: "1em",
                 align: { x: "center", y: "center" }
             },        
@@ -34,7 +37,7 @@ const StockAvailablityReportPdfCtrl = async ( req, res, next ) => {
         [
             {
                 text: `Total Available Medicines: ${ medicinesRecords.length }`,
-                colSpan: "3",
+                colSpan: "4",
                 padding: "1em",
                 align: { x: "center", y: "center" }
             }
@@ -61,6 +64,13 @@ const StockAvailablityReportPdfCtrl = async ( req, res, next ) => {
                 padding: "1em",
                 align: { x: "center", y: "center" }
             },
+            {
+                text: "Total Price",
+                backgroundColor: "#777",
+                textColor: "#fff",
+                padding: "1em",
+                align: { x: "center", y: "center" }
+            }
         ]
     ];
 
@@ -80,11 +90,18 @@ const StockAvailablityReportPdfCtrl = async ( req, res, next ) => {
                 text: singleMedicineRecord.medicinePrice,
                 padding: "0.7em",
                 align: { x: "center", y: "center" }
-            }, 
+            },
+            {
+                text: singleMedicineRecord.totalPrice,
+                padding: "0.7em",
+                align: { x: "center", y: "center" }
+            }
         ];
         
         medicinesStocksArray.push( individualRecordObjectToArray );
     } )
+
+    console.log( medicinesStocksArray );
 
     let doc = new PDFDocument();
 
