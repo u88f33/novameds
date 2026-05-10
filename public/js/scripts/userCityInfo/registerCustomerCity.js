953ce3eb04944a8e1513758fad4f1e0ddb57c8ca
Muhammad Umar <u88f33@gmail.com>
2026-05-10 12:23:58 +0500

    Changes done

--- public/js/scripts/userCityInfo/registerCustomerCity.js
+++ public/js/scripts/userCityInfo/registerCustomerCity.js
@@ -2,7 +2,15 @@ import { cities } from "./userCities.js";
 
 let options = ``;
 cities.forEach(city => {
-    options += `<option value="${city.name}">${ city.name }</option>`;
+    
+    let customerCityInDatabase = citySelectedByCustomer || "";
+
+    if ( customerCityInDatabase == city.name ) {
+        options += `<option value="${city.name}" selected='selected'>${ city.name }</option>`;
+    } else {
+        options += `<option value="${city.name}">${ city.name }</option>`;
+    }
+
 });
 
 
