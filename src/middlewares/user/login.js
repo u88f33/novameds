b6ff093568ca7f309301a8e5cda627f942d466c8
Muhammad Umar <u88f33@gmail.com>
2026-06-20 21:00:41 +0500

    Changes done

--- src/middlewares/user/login.js
+++ src/middlewares/user/login.js
@@ -7,18 +7,18 @@ dotenv.config();
 const UserLoginMiddleware = ( req, res, next ) => {
 
     try {
-
+        
         const token = jwt.verify( 
             req.cookies.userToken,
             process.env.JWT_SECRET_KEY  
         );
-
+        
         if ( token ) {
             next();
         }
-
+        
     } catch ( err ) {
-
+        
         res.redirect( "/login" );
         console.log( `Error while login: ${err.message}` );
         
