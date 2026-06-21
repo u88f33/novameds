d73656317c7c80f80f5edb26e66b236889b3f7fc
Muhammad Umar <u88f33@gmail.com>
2026-06-21 16:59:58 +0500

    Changes done

--- src/controllers/auth/login/login_get.controller.js
+++ src/controllers/auth/login/login_get.controller.js
@@ -2,14 +2,29 @@ import MedicineCollection from "../../../models/medicines.model.js";
 
 const LoginGetCtrl = async ( req, res, next ) => {
 
-    const medicineRecords = await MedicineCollection.find();
+    try {
+        console.log( "**************************************" );
+        console.log( "Cookies on Login Page" );
+        console.log( "**************************************" );
+        console.log( req.cookies );
 
-    res.render( "auth/login",
-        { 
-            medicineRecords,
-            errorMessage: req.query.errorMessage
-        }
-     );
+        const medicineRecords = await MedicineCollection.find();
+
+        res.render( "auth/login",
+            { 
+                medicineRecords,
+                errorMessage: req.query.errorMessage
+            }
+        );
+
+
+    } catch ( err ) {
+
+        console.log( "/src/controllers/auth/login/login_get.controller.js" );
+        console.log( `Error: ${ err }` );
+
+    }
+    
 }
 
 export default LoginGetCtrl;
