594a994e756cb58bdfbe2119b7668364eb63c4a2
Muhammad Umar <u88f33@gmail.com>
2026-07-10 18:58:17 +0500

    Changes done

--- src/routes/auth/oauth/google/google-auth.route.js
+++ src/routes/auth/oauth/google/google-auth.route.js
@@ -3,7 +3,7 @@ import passport from "passport";
 const router = express.Router();
 
 router.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile'] }));
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
