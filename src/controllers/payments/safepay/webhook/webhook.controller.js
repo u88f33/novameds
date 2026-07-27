74e010d79a14101ff2129a80dc39608e7e308807
Muhammad Umar <u88f33@gmail.com>
2026-07-27 16:59:34 +0500

    Changes done

--- src/controllers/payments/safepay/webhook/webhook.controller.js
+++ src/controllers/payments/safepay/webhook/webhook.controller.js
@@ -1,13 +1,21 @@
-import generateSafepayWebhook from
+import { generateSafepayWebhook } from
 "../../../../utils/safepay/safepay.js";
 
 const WebhookBySafepayServer = async ( req, res, next ) => {
-    const valid = await generateSafepayWebhook( req );
+    try {
 
-    console.log( "Webhook response by Safepay" )
-    console.log( "------------------------------------------------------" );
-    console.log( valid );
-    console.log( "------------------------------------------------------" );
+        const valid = await generateSafepayWebhook( req );
+
+        console.log( "Webhook response by Safepay" )
+        console.log( "------------------------------------------------------" );
+        console.log( valid );
+        console.log( "------------------------------------------------------" );
+
+    } catch ( err ) {
+        console.log( "Error in controllers/payments/safepay/webhook/webhook.controller.js" );
+        console.log( `Error: ${ err }` );
+        console.log( `Error Message: ${ err.message }` );
+    }
 }
 
 export default WebhookBySafepayServer;
