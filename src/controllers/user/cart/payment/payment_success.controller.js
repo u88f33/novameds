19153e5074ee7b202129971f39ea62ae7ecfc379
Muhammad Umar <u88f33@gmail.com>
2026-07-28 21:48:23 +0500

    Changes done

--- src/controllers/user/cart/payment/payment_success.controller.js
+++ src/controllers/user/cart/payment/payment_success.controller.js
@@ -1,5 +1,8 @@
 import medicineRecordsArray from "../../../../utils/medicines/records.js";
+import MedicineCollection from "../../../../models/medicines.model.js";
 import OrderCollection from "../../../../models/order.model.js";
+import CartCollection from "../../../../models/cart.model.js";
+import { newOrder } from "../checkout/checkout.controller.js";
 
 const paymentSuccessCtrl = async ( req, res, next ) => {
     
@@ -9,7 +12,79 @@ const paymentSuccessCtrl = async ( req, res, next ) => {
 
     const customerOrder = await OrderCollection.findById( req.params.id )
     .populate( "customerId" );
+
+    const items = await CartCollection.find( 
+        { customerId },
+        {
+            _id: 0,
+            medicineId: 1,
+            price: 1,
+            quantity: 1
+        }
+    );
+
+    let totalItems = items.length;
     
+    /**
+     * Iterate throught the "items" array containing cart items and Update
+     * the medicine stock in the Database
+     */
+    for ( let i = 0; i < totalItems; ++i ) {
+        // Fetch the medicine by its _id.
+        let medicineDetails = 
+        await MedicineCollection.findById( items[i].medicineId );
+
+        // Update the Medicines stock in the database.
+        let updatedStock = medicineDetails.medicineStock - items[i].quantity;
+        
+        // Update stock on Confirming Order by Customer.
+        if ( medicineDetails ) {
+            await MedicineCollection.updateOne(
+                { _id: items[i].medicineId },
+                {
+                    $set: { medicineStock: updatedStock }
+                }
+            )
+        }
+    }
+
+    if ( totalItems <= 0 ) {
+        return res.redirect( '/profile/cart/checkout/?emptyCartMsg=Cart is Empty' );
+    }
+
+    let totalAmount = 0;
+    for ( let i = 0; i < totalItems; ++i ) {
+        totalAmount += items[i].price;
+    }
+
+    let deliveryCharges = 300;
+    totalAmount = totalAmount + deliveryCharges;
+
+    newOrder.customerId = customerId;
+    newOrder.items = items;
+    newOrder.orderStatus = "Pending",
+    newOrder.totalAmount = totalAmount
+
+
+    const insertDataInMongoDB = await newOrder.save();
+
+
+    const confirmedOrderDetails = await OrderCollection.findById(
+        insertDataInMongoDB._id
+    ).populate( "customerId" ).populate( "items.medicineId" );
+
+    if ( insertDataInMongoDB ) {
+        
+        const deleteCustomerCartItems = await CartCollection.deleteMany( { 
+            customerId
+        } );
+        
+        const orderId = insertDataInMongoDB._id;
+        const orderDetails = confirmedOrderDetails;
+
+        generateInvoice( orderId, res, orderDetails );
+    }
+
     res.render(
         "user/paymentSuccess",
         {
