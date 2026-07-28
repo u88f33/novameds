09579ebc7c5c80aa4261fbc38e69882f615162d5
Muhammad Umar <u88f33@gmail.com>
2026-07-28 21:56:48 +0500

    Changes done

--- src/controllers/user/cart/payment/payment_success.controller.js
+++ src/controllers/user/cart/payment/payment_success.controller.js
@@ -2,6 +2,7 @@ import medicineRecordsArray from "../../../../utils/medicines/records.js";
 import MedicineCollection from "../../../../models/medicines.model.js";
 import OrderCollection from "../../../../models/order.model.js";
 import CartCollection from "../../../../models/cart.model.js";
+import generateInvoice from "../../../../utils/invoice/pdfGenerator.js";
 import { newOrder } from "../checkout/checkout.controller.js";
 
 const paymentSuccessCtrl = async ( req, res, next ) => {
