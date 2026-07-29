aff1b6d22d75de1f26b106504ef240a8dde10825
Muhammad Umar <u88f33@gmail.com>
2026-07-29 16:59:08 +0500

    Changes done

--- src/controllers/payments/safepay/webhook/webhook.controller.js
+++ src/controllers/payments/safepay/webhook/webhook.controller.js
@@ -13,93 +13,100 @@ const WebhookBySafepayServer = async ( req, res, next ) => {
         const valid = await generateSafepayWebhook( req );
         const orderId = req.body.data.notification.metadata.order_id;
 
+        
 
         if ( valid ) {
-            const medicineRecords = await medicineRecordsArray();
-            const customerName = req.session.userLoginSession.userName;
-            const customerId = req.session.userLoginSession.userId;
 
-            const customerOrder = await OrderCollection.findById( orderId )
-            .populate( "customerId" );
+            console.log( "---------------------------" );
+            console.log( "Session in Webhook" )
+            console.log( "---------------------------" );
+            console.log( req.session );
 
-            const items = await CartCollection.find( 
-                { customerId },
-                {
-                    _id: 0,
-                    medicineId: 1,
-                    price: 1,
-                    quantity: 1
-                }
-            );
+        //     const medicineRecords = await medicineRecordsArray();
+        //     const customerName = req.session.userLoginSession.userName;
+        //     const customerId = req.session.userLoginSession.userId;
 
-            let totalItems = items.length;
+        //     const customerOrder = await OrderCollection.findById( orderId )
+        //     .populate( "customerId" );
+
+        //     const items = await CartCollection.find( 
+        //         { customerId },
+        //         {
+        //             _id: 0,
+        //             medicineId: 1,
+        //             price: 1,
+        //             quantity: 1
+        //         }
+        //     );
+
+        //     let totalItems = items.length;
             
-            /**
-             * Iterate throught the "items" array containing cart items and Update
-             * the medicine stock in the Database
-             */
-            for ( let i = 0; i < totalItems; ++i ) {
-                // Fetch the medicine by its _id.
-                let medicineDetails = 
-                await MedicineCollection.findById( items[i].medicineId );
+        //     /**
+        //      * Iterate throught the "items" array containing cart items and Update
+        //      * the medicine stock in the Database
+        //      */
+        //     for ( let i = 0; i < totalItems; ++i ) {
+        //         // Fetch the medicine by its _id.
+        //         let medicineDetails = 
+        //         await MedicineCollection.findById( items[i].medicineId );
 
-                // Update the Medicines stock in the database.
-                let updatedStock = medicineDetails.medicineStock - items[i].quantity;
+        //         // Update the Medicines stock in the database.
+        //         let updatedStock = medicineDetails.medicineStock - items[i].quantity;
                 
-                // Update stock on Confirming Order by Customer.
-                if ( medicineDetails ) {
-                    await MedicineCollection.updateOne(
-                        { _id: items[i].medicineId },
-                        {
-                            $set: { medicineStock: updatedStock }
-                        }
-                    )
-                }
-            }
+        //         // Update stock on Confirming Order by Customer.
+        //         if ( medicineDetails ) {
+        //             await MedicineCollection.updateOne(
+        //                 { _id: items[i].medicineId },
+        //                 {
+        //                     $set: { medicineStock: updatedStock }
+        //                 }
+        //             )
+        //         }
+        //     }
 
-            if ( totalItems <= 0 ) {
-                return res.redirect( '/profile/cart/checkout/?emptyCartMsg=Cart is Empty' );
-            }
+        //     if ( totalItems <= 0 ) {
+        //         return res.redirect( '/profile/cart/checkout/?emptyCartMsg=Cart is Empty' );
+        //     }
 
-            let totalAmount = 0;
-            for ( let i = 0; i < totalItems; ++i ) {
-                totalAmount += items[i].price;
-            }
+        //     let totalAmount = 0;
+        //     for ( let i = 0; i < totalItems; ++i ) {
+        //         totalAmount += items[i].price;
+        //     }
 
-            let deliveryCharges = 300;
-            totalAmount = totalAmount + deliveryCharges;
+        //     let deliveryCharges = 300;
+        //     totalAmount = totalAmount + deliveryCharges;
 
-            newOrder.customerId = customerId;
-            newOrder.items = items;
-            newOrder.orderStatus = "Pending";
-            newOrder.paymentStatus = "Paid";
-            newOrder.paymentMethod = "Safepay";
-            newOrder.totalAmount = totalAmount;
-
-
-            const insertDataInMongoDB = await newOrder.save();
+        //     newOrder.customerId = customerId;
+        //     newOrder.items = items;
+        //     newOrder.orderStatus = "Pending";
+        //     newOrder.paymentStatus = "Paid";
+        //     newOrder.paymentMethod = "Safepay";
+        //     newOrder.totalAmount = totalAmount;
 
 
-            const confirmedOrderDetails = await OrderCollection.findById(
-                insertDataInMongoDB._id
-            ).populate( "customerId" ).populate( "items.medicineId" );
+        //     const insertDataInMongoDB = await newOrder.save();
 
-            if ( insertDataInMongoDB ) {
+
+        //     const confirmedOrderDetails = await OrderCollection.findById(
+        //         insertDataInMongoDB._id
+        //     ).populate( "customerId" ).populate( "items.medicineId" );
+
+        //     if ( insertDataInMongoDB ) {
                 
-                const deleteCustomerCartItems = await CartCollection.deleteMany( { 
-                    customerId
-                } );
+        //         const deleteCustomerCartItems = await CartCollection.deleteMany( { 
+        //             customerId
+        //         } );
                 
-                const orderId = insertDataInMongoDB._id;
-                const orderDetails = confirmedOrderDetails;
+        //         const orderId = insertDataInMongoDB._id;
+        //         const orderDetails = confirmedOrderDetails;
 
-                console.log( orderId );
-                console.log( orderDetails );
+        //         console.log( orderId );
+        //         console.log( orderDetails );
 
-                generateInvoice( orderId, res, orderDetails );
-            }
+        //         generateInvoice( orderId, res, orderDetails );
+        //     }
 
-            res.redirect( `/profile/cart/order/confirm/${ confirmedOrderDetails._id }` );
+        //     res.redirect( `/profile/cart/order/confirm/${ confirmedOrderDetails._id }` );
         }
 
     } catch ( err ) {
