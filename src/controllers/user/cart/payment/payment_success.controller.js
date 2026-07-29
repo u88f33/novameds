529eb783205b4052e1f7946794d3bfcf65503ff8
Muhammad Umar <u88f33@gmail.com>
2026-07-29 16:36:00 +0500

    Changes done

--- src/controllers/user/cart/payment/payment_success.controller.js
+++ src/controllers/user/cart/payment/payment_success.controller.js
@@ -8,96 +8,19 @@ import { newOrder } from "../checkout/checkout.controller.js";
 const paymentSuccessCtrl = async ( req, res, next ) => {
 
     try {
-        const medicineRecords = await medicineRecordsArray();
-        const customerName = req.session.userLoginSession.userName;
-        const customerId = req.session.userLoginSession.userId;
-
-        const customerOrder = await OrderCollection.findById( req.params.id )
-        .populate( "customerId" );
-
-        const items = await CartCollection.find( 
-            { customerId },
+        res.render(
+            "user/paymentSuccess",
             {
-                _id: 0,
-                medicineId: 1,
-                price: 1,
-                quantity: 1
+                customerOrder,
+                medicineRecords,
+                nameOfLoggedInUser: customerName,
+                loggedInUserId: customerId
             }
-        );
-
-        let totalItems = items.length;
-        
-        /**
-         * Iterate throught the "items" array containing cart items and Update
-         * the medicine stock in the Database
-         */
-        for ( let i = 0; i < totalItems; ++i ) {
-            // Fetch the medicine by its _id.
-            let medicineDetails = 
-            await MedicineCollection.findById( items[i].medicineId );
-
-            // Update the Medicines stock in the database.
-            let updatedStock = medicineDetails.medicineStock - items[i].quantity;
-            
-            // Update stock on Confirming Order by Customer.
-            if ( medicineDetails ) {
-                await MedicineCollection.updateOne(
-                    { _id: items[i].medicineId },
-                    {
-                        $set: { medicineStock: updatedStock }
-                    }
-                )
-            }
-        }
-
-        if ( totalItems <= 0 ) {
-            return res.redirect( '/profile/cart/checkout/?emptyCartMsg=Cart is Empty' );
-        }
-
-        let totalAmount = 0;
-        for ( let i = 0; i < totalItems; ++i ) {
-            totalAmount += items[i].price;
-        }
-
-        let deliveryCharges = 300;
-        totalAmount = totalAmount + deliveryCharges;
-
-        newOrder.customerId = customerId;
-        newOrder.items = items;
-        newOrder.orderStatus = "Pending";
-        newOrder.paymentStatus = "Paid";
-        newOrder.paymentMethod = "Safepay";
-        newOrder.totalAmount = totalAmount;
-
-
-        const insertDataInMongoDB = await newOrder.save();
-
-
-        const confirmedOrderDetails = await OrderCollection.findById(
-            insertDataInMongoDB._id
-        ).populate( "customerId" ).populate( "items.medicineId" );
-
-        if ( insertDataInMongoDB ) {
-            
-            const deleteCustomerCartItems = await CartCollection.deleteMany( { 
-                customerId
-            } );
-            
-            const orderId = insertDataInMongoDB._id;
-            const orderDetails = confirmedOrderDetails;
-
-            console.log( orderId );
-            console.log( orderDetails );
-
-            generateInvoice( orderId, res, orderDetails );
-        }
-
-        res.redirect( `/profile/cart/order/confirm/${ confirmedOrderDetails._id }` );
-
+        )
     } catch ( err ) {
         console.log( "Error in /controllers/user/cart/payment/payment_success.controller.js" );
         console.log( "-------------------------------------------" );
-        console.log( err.message );
+        console.log( err );
         console.log( "-------------------------------------------" );
     }
 
